2e3dd42e4655aa209fc1c9930d8394f9b0c5ea6f
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2024-05-26 20:07:49 -0500

    Add support for cargo config

--- src/workspace.rs
+++ src/workspace.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::{HashMap, VecDeque},
+    path,
 };
 
 use crate::{
@@ -34,15 +35,19 @@ pub fn create(printer: &mut Printer, space_name: &String) -> anyhow::Result<()> 
         let section = printer::Heading::new(heading.printer, "Workspace")?;
         let mut execute_batch = printer::ExecuteBatch::new();
         for (spaces_key, dependency) in workspace.repositories.iter() {
-            let (bare_repository, execute_later) = git::BareRepository::new(section.printer, spaces_key, &dependency.git)?;
+            let (bare_repository, execute_later) =
+                git::BareRepository::new(section.printer, spaces_key, &dependency.git)?;
             execute_batch.add(spaces_key, execute_later);
 
             section.printer.info(spaces_key, &bare_repository)?;
 
-            let (worktree, execute_later) = bare_repository.add_worktree(section.printer, &directory)?;
+            let (worktree, execute_later) =
+                bare_repository.add_worktree(section.printer, &directory)?;
             execute_batch.add(spaces_key, execute_later);
-            execute_batch.add(spaces_key, worktree.switch_new_branch(section.printer, dependency)?);
-
+            execute_batch.add(
+                spaces_key,
+                worktree.switch_new_branch(section.printer, dependency)?,
+            );
         }
         execute_batch.execute(section.printer)?;
     }
@@ -52,7 +57,6 @@ pub fn create(printer: &mut Printer, space_name: &String) -> anyhow::Result<()> 
     if let Some(buck) = &workspace_config.buck {
         buck.export(&directory)?;
     }
-
 
     state.sync_full_path()?;
 
@@ -64,7 +68,6 @@ pub fn sync(printer: &mut Printer) -> anyhow::Result<()> {
     let mut state = State::new(printer, &full_path)?;
     state.sync_full_path()
 }
-
 
 struct State<'a> {
     printer: &'a mut Printer,
@@ -81,7 +84,7 @@ impl<'a> State<'a> {
             full_path,
             workspace: Workspace::new(full_path)?,
             deps_map: HashMap::new(),
-            all_deps: VecDeque::new()
+            all_deps: VecDeque::new(),
         })
     }
 
@@ -89,6 +92,7 @@ impl<'a> State<'a> {
         self.sync_repositories()?;
         self.sync_dependencies()?;
         self.export_buck_config()?;
+        self.update_cargo()?;
         Ok(())
     }
 
@@ -99,10 +103,10 @@ impl<'a> State<'a> {
             self.deps_map.insert(spaces_key.clone(), dependency.clone());
             let (bare_repository, execute_later) =
                 git::BareRepository::new(section.printer, spaces_key, &dependency.git)?;
-            
+
             section.printer.info(spaces_key, &bare_repository)?;
             execute_batch.add(spaces_key, execute_later);
-            
+
             self.all_deps
                 .push_back((bare_repository, dependency.clone()));
         }
@@ -115,11 +119,11 @@ impl<'a> State<'a> {
         let heading = printer::Heading::new(self.printer, "Dependencies")?;
         loop {
             if let Some((bare_repository, dependency)) = self.all_deps.pop_back() {
-
                 let mut execute_batch = printer::ExecuteBatch::new();
 
                 let section = printer::Section::new(heading.printer, &bare_repository.spaces_key)?;
-                let (worktree, execute_later) = bare_repository.add_worktree(section.printer, self.full_path)?;
+                let (worktree, execute_later) =
+                    bare_repository.add_worktree(section.printer, self.full_path)?;
                 execute_batch.add(bare_repository.spaces_key.as_str(), execute_later);
 
                 if self.deps_map.contains_key(&bare_repository.spaces_key) {
@@ -127,16 +131,23 @@ impl<'a> State<'a> {
                 } else {
                     self.deps_map
                         .insert(bare_repository.spaces_key.clone(), dependency.clone());
-                    execute_batch.add(&bare_repository.spaces_key, worktree.checkout(section.printer, &dependency)?);
-                    execute_batch.add(&bare_repository.spaces_key, worktree.checkout_detached_head(section.printer)?);
+                    execute_batch.add(
+                        &bare_repository.spaces_key,
+                        worktree.checkout(section.printer, &dependency)?,
+                    );
+                    execute_batch.add(
+                        &bare_repository.spaces_key,
+                        worktree.checkout_detached_head(section.printer)?,
+                    );
                 }
 
                 let spaces_deps = worktree.get_deps()?;
                 if let Some(spaces_deps) = spaces_deps {
                     section.printer.info("deps", &spaces_deps)?;
-                    for (key, dep) in spaces_deps.deps.iter() {
-                        section.printer.info(key, dep)?;
-                        let (bare_repository, execute_later) = git::BareRepository::new(section.printer, key, &dep.git)?;
+                    for (spaces_key, dep) in spaces_deps.deps.iter() {
+                        section.printer.info(spaces_key, dep)?;
+                        let (bare_repository, execute_later) =
+                            git::BareRepository::new(section.printer, spaces_key, &dep.git)?;
 
                         execute_batch.add(bare_repository.spaces_key.as_str(), execute_later);
                         if !self
@@ -202,7 +213,6 @@ impl<'a> State<'a> {
                             archive.create_soft_link(self.full_path)?;
                         }
                     }
-
                 } else {
                     section.printer.info("deps", &"No dependencies found")?;
                 }
@@ -219,7 +229,6 @@ impl<'a> State<'a> {
         let workspace = &mut self.workspace;
         let deps_map = &mut self.deps_map;
 
-        let heading = printer::Heading::new(self.printer, "Configuration")?;
         if let Some(buck) = workspace.buck.as_mut() {
             if buck.cells.is_none() {
                 buck.cells = Some(HashMap::new());
@@ -231,13 +240,56 @@ impl<'a> State<'a> {
                 }
             }
             buck.export(self.full_path)?;
-        } else {
-            heading.printer.info("buck", &"No buck config found")?;
         }
 
-        heading.printer.debug("workspace", &workspace)?;
-
         workspace.save(self.full_path)?;
+
+        Ok(())
+    }
+
+    fn update_cargo(&mut self) -> anyhow::Result<()> {
+        if let Some(cargo) = self.workspace.get_cargo_patches() {
+            let heading = printer::Heading::new(self.printer, "Cargo")?;
+
+            heading.printer.info("cargo", cargo)?;
+
+            for (spaces_key, list) in cargo.iter() {
+                //read the cargo toml file to see how the dependency is specified crates-io or git
+                let cargo_toml_path = format!("{}/{spaces_key}/Cargo.toml", self.full_path);
+                let mut cargo_toml_contents = std::fs::read_to_string(&cargo_toml_path)?;
+                let cargo_toml: toml::Value = toml::from_str(&cargo_toml_contents)?;
+
+                let dependencies = cargo_toml.get("dependencies").ok_or(anyhow::anyhow!(
+                    "Cargo.toml does not have a dependencies section"
+                ))?;
+
+                const START_WORKSPACE: &str = "\n\n#! spaces_workspace\n";
+                const END_WORKSPACE: &str = "#! drop(spaces_workspace)\n";
+
+                if let (Some(start), Some(end)) = (cargo_toml_contents.find(START_WORKSPACE), cargo_toml_contents.find(END_WORKSPACE)) {
+                    cargo_toml_contents.replace_range(start..(end + END_WORKSPACE.len()), "");
+                }
+
+                cargo_toml_contents.push_str(START_WORKSPACE);
+
+                for value in list {
+                    let dependency = dependencies.get(value.as_str()).ok_or(anyhow::anyhow!(
+                        "Cargo.toml does not have a dependency named {}",
+                        value
+                    ))?;
+
+                    if let Some(git) = dependency.get("git").map(|e| e.as_str()).flatten() {
+                        let patch = format!("[patch.'{}']\n", git);
+                        let path = format!("{value} = {{ path = \"../{value}\" }}\n");
+                        cargo_toml_contents.push_str(patch.as_str());
+                        cargo_toml_contents.push_str(path.as_str());
+                    }
+                }
+                cargo_toml_contents.push_str(END_WORKSPACE);
+
+                std::fs::write(&cargo_toml_path, cargo_toml_contents)?;
+            }
+        }
 
         Ok(())
     }
