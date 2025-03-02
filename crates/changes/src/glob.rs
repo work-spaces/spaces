f9c602650e71a04ef2560196569599e1ebcd445d
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2025-03-02 12:39:33 -0700

    Issue #85. Add tests to crates/changes/globs and enable CI tests

--- crates/changes/src/glob.rs
+++ crates/changes/src/glob.rs
@@ -61,3 +61,145 @@ pub fn validate(globs: &HashSet<Arc<str>>) -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rule_names() {
+        static INPUT_LIST: &[&str] = &[
+            "//capsules:ftp.gnu.org-m4-m4-exp_relocate_bin",
+            "//capsules:ftp.gnu.org-libidn-libidn2-exp_publish_archive",
+            "//capsules:github.com-lz4-lz4-exp_cmake_configure",
+            "//capsules:github.com-madler-zlib-exp_relocate_bin",
+            "//capsules:github.com-ngtcp2-nghttp3-exp_relocate_bin",
+            "//capsules:relocate",
+            "//capsules:bin",
+            "//:all",
+            "//:setup",
+            "//test/capsules/test1:configure",
+            "//test/capsules/test1:build",
+            "//test/capsules/test1:install",
+            "//test/capsules/test1:install_bin",
+        ];
+
+        let globs = vec!["+//**/*:*bin".into()].into_iter().collect();
+        assert_eq!(match_globs(&globs, INPUT_LIST[0]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[1]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[2]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[3]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[4]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[5]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[7]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[8]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[9]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[10]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[11]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[12]), true);
+
+        let globs = vec!["+//**/*:*bin".into(), "-//**/*:*ftp.gnu.org*".into()]
+            .into_iter()
+            .collect();
+        assert_eq!(match_globs(&globs, INPUT_LIST[0]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[1]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[2]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[3]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[4]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[5]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[6]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[7]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[8]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[9]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[10]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[11]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[12]), true);
+        let globs = vec!["+//**/**".into(), "-//capsules:*".into()]
+            .into_iter()
+            .collect();
+        assert_eq!(match_globs(&globs, INPUT_LIST[0]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[1]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[2]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[3]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[4]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[5]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[6]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[7]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[8]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[9]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[10]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[11]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[12]), true);
+        let globs = vec!["+//**/capsules/**:*".into()].into_iter().collect();
+        assert_eq!(match_globs(&globs, INPUT_LIST[0]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[1]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[2]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[3]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[4]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[5]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[6]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[7]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[8]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[9]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[10]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[11]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[12]), true);
+        let globs = vec![
+            "+//*/capsules/**:*".into(),
+            "-//*/capsules/**:*install*".into(),
+        ]
+        .into_iter()
+        .collect();
+        assert_eq!(match_globs(&globs, INPUT_LIST[0]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[1]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[2]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[3]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[4]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[5]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[6]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[7]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[8]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[9]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[10]), true);
+        assert_eq!(match_globs(&globs, INPUT_LIST[11]), false);
+        assert_eq!(match_globs(&globs, INPUT_LIST[12]), false);
+    }
+
+    #[test]
+    fn test_is_glob_include() {
+        assert_eq!(is_glob_include("+foo"), Some("foo".into()));
+        assert_eq!(is_glob_include("-foo"), None);
+        assert_eq!(is_glob_include("+"), Some(".".into()));
+        assert_eq!(is_glob_include("-"), None);
+    }
+
+    #[test]
+    fn test_match_globs() {
+        let globs = vec!["+foo".into(), "-foo".into(), "+bar".into()]
+            .into_iter()
+            .collect();
+
+        assert_eq!(match_globs(&globs, "foo"), false);
+        assert_eq!(match_globs(&globs, "bar"), true);
+        assert_eq!(match_globs(&globs, "baz"), false);
+    }
+
+    #[test]
+    fn test_validate() {
+        let globs = vec!["+foo".into(), "-foo".into(), "+bar".into(), "-bar".into()]
+            .into_iter()
+            .collect();
+
+        assert!(validate(&globs).is_ok());
+
+        let globs = vec!["foo".into()].into_iter().collect();
+        assert!(validate(&globs).is_err());
+
+        let globs = vec!["-foo".into()].into_iter().collect();
+        assert!(validate(&globs).is_err());
+
+        let globs = vec!["-foo".into(), "-bar".into()].into_iter().collect();
+        assert!(validate(&globs).is_err());
+    }
+}
