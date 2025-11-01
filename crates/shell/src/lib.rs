e640a1b94394093de78893a7b3c6db60d8335544
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2025-11-01 08:16:26 -0600

    Issue #232. Add shell shortcuts generator

--- crates/shell/src/lib.rs
+++ crates/shell/src/lib.rs
@@ -1,7 +1,40 @@
 use anyhow::Context;
-use anyhow_source_location::format_context;
+use anyhow_source_location::{format_context, format_error};
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::sync::Arc;
+
+enum ShellType {
+    Bash,
+    Zsh,
+    Fish,
+}
+
+impl TryFrom<Arc<str>> for ShellType {
+    type Error = anyhow::Error;
+
+    fn try_from(shell: Arc<str>) -> Result<Self, Self::Error> {
+        match shell.as_ref() {
+            "bash" => Ok(ShellType::Bash),
+            "zsh" => Ok(ShellType::Zsh),
+            "fish" => Ok(ShellType::Fish),
+            _ => {
+                if shell.ends_with("bash") {
+                    Ok(ShellType::Bash)
+                } else if shell.ends_with("zsh") {
+                    Ok(ShellType::Zsh)
+                } else if shell.ends_with("fish") {
+                    Ok(ShellType::Fish)
+                } else {
+                    Err(format_error!(
+                        "Unsupported shell type: {} - use path to: bash, zsh, fish",
+                        shell
+                    ))
+                }
+            }
+        }
+    }
+}
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Startup {
@@ -15,6 +48,7 @@ pub struct Config {
     pub path: Arc<str>,
     pub startup: Option<Startup>,
     pub args: Vec<Arc<str>>,
+    pub shortcuts: Option<HashMap<Arc<str>, Arc<str>>>,
 }
 
 impl Config {
@@ -40,9 +74,30 @@ impl Config {
                 path: default_shell_path.unwrap_or("/bin/bash".into()),
                 startup: None,
                 args: Vec::new(),
+                shortcuts: None,
             })
         }
     }
+}
+
+fn create_shortcuts(
+    path: Arc<str>,
+    shortcuts: &HashMap<Arc<str>, Arc<str>>,
+) -> anyhow::Result<Vec<Arc<str>>> {
+    let shell_type = ShellType::try_from(path.clone())
+        .context(format_context!("while decoding shell type from {}", path))?;
+
+    let mut functions = Vec::new();
+
+    for (key, value) in shortcuts {
+        let function = match shell_type {
+            ShellType::Bash | ShellType::Zsh => format!("{key}() {{\n\t{value}\n }}"),
+            ShellType::Fish => format!("function {key}\n\t{value}\nend"),
+        };
+        functions.push(function.into());
+    }
+
+    Ok(functions)
 }
 
 pub fn run(
@@ -53,6 +108,18 @@ pub fn run(
     // Create the command
     let mut process = std::process::Command::new(config.path.as_ref());
     process.env_clear();
+
+    if let Some(shortcuts) = config.shortcuts.as_ref() {
+        let shortcuts = create_shortcuts(config.path.clone(), shortcuts)
+            .context(format_context!("While creating shortcuts"))?;
+        let mut contents = shortcuts.join("\n\n");
+        contents.push_str("\n\n");
+        let shortcuts_file = startup_directory.join("shortcuts.sh");
+        std::fs::write(&shortcuts_file, contents).context(format_context!(
+            "Failed to write shell shortcuts file to {}",
+            shortcuts_file.display()
+        ))?;
+    }
 
     if let Some(startup) = config.startup.as_ref() {
         let config_file = startup_directory.join(startup.name.as_ref());
