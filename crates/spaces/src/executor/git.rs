3bbdddbb8f70b9694e9fa0b843139a2c8b9f8337
Tyler <tyler-gilbert@users.noreply.github.com>
2026-07-25 14:39:16 -0600

    #1088. Update sync with support for submodules during pull (#1097)
    * #1088. Update sync with support for submodules during pull
    
    * #1088. Improve calls to git for pull

--- crates/spaces/src/executor/git.rs
+++ crates/spaces/src/executor/git.rs
@@ -323,7 +323,7 @@ impl Git {
 
                 let existing_repo = git::Repository::new(self.url.clone(), self.spaces_key.clone());
 
-                if existing_repo.is_dirty(progress, git::IgnoreSubmodules::No) {
+                if existing_repo.is_dirty(progress, git::IgnoreSubmodules::Yes) {
                     logger(progress.console.clone(), self.url.clone()).warning(
                         format!(
                             "{} already exists and is dirty - not updating",
@@ -359,9 +359,11 @@ impl Git {
                 if existing_repo.is_head_branch(progress)
                     && existing_repo.is_remote_branch_tracked(progress)
                 {
-                    existing_repo.pull(progress).context(format_context!(
-                        "while pulling latest changes in existing workspace"
-                    ))?;
+                    existing_repo
+                        .pull(progress, git::IsRecurseSubmodules::No)
+                        .context(format_context!(
+                            "while pulling latest changes in existing workspace"
+                        ))?;
                 }
 
                 return Ok(());
